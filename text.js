113fb087bfe48c1c4c42d28f277dcc4d988b3622
andreirfrancisco <132955328+andreirfrancisco@users.noreply.github.com>
2023-09-20 15:39:21 -0700

    Update text.js
    Add js to home page.

--- text.js
+++ text.js
@@ -1,59 +1,13 @@
-// topics 
+export const intrdoction = ["Welcome to Andrei's Page", "Here you can learn more about me, handle MySQl tasks, clean Pandas dataframes, and create own dashboard" ];
+export const icon_to_sub = new Map([
+    [1, ["Education", "Skills", "Projects"]],
+    [2, ["Database Administration", "Queries", "Conversions", "Update Database"]],
+    [3, ["Libraries/Programs Needed", "Cleaning Data", "Statistics/Figures", "Model Preparation"]],
+    [4, ["Libraries/Programs Needed", "HTML", "Styles", "Create Dashboard"]]
+]);
+export const sub_to_main = new Map([ 
+[1, ["UCLA Economics B.A", "June 2023" , "Coursera IBM Data Analytics", "August 2023"]], 
+[2, ["Python", "R", "Tableau", "Javascript", "Microsoft Office", "Statistics"]],
+[3, ["Text", "File"]]
+]);
 
-export const tabs_1 = ["Education", "Skills", "Projects" ];
-export const tabs_2 = ["Database administration", "Queries", "Convertions", "Update Database" ];
-export const tabs_3 = ["Libraries Needed", "Cleaning", "Statistics/Figures", "Model Preparation" ];
-export const tabs_4 = ["Libraries Needed", "HTML", "Styles" , "Create Dashboard"];
-// actual subjects
-// education 
-export const education = {
-    "UCLA Economics B.A": "June 2023",
-    "Coursera IBM Data Analytics": "August 2023"
-};
-
-// projects
-export const projects = {
-    "Personal Website": "September 2023",
-    "Users can obtain MySQL Queries, Clean Pandas Dataframes, and Create Dashboard": "Languages used: Javascript, HTML, CSS, and Python",
-    "Past Data Analysis Reports": "January 2022 - June 2023",
-    "All reports related to data analysis can be found": "here"
-};
-
-// skills
-let year_2 = "2 Years experience";
-let year_1 = "1 Year experience";
-
-export const skills = {
-    "Python": year_2, "R": year_2,
-    "Tableau": year_1, "Microsoft Office": year_2,
-    "MySQL": year_1,
-};
-
-// sql queries 
-export const queries = [
-    "inner", "left", "right", "outer"
-];
-
-export const queries_update = [
-    "select", "update", "delete", "condition"
-];
-
-export const queries_function = [
-    "max", "min", "max", "range", "asc", "desc"
-];
-
-// cleaning python dataframes
-export const regression = [
-    "Linear", "Quasi", "Log"
-];
-
-// need to add
-export const classification = [];
-
-// need to add
-export const clustering = [];
-
-// need to add
-export const plots = ["bar",
-    "box", "2d ", "3d", "scatter", "time",
-];
