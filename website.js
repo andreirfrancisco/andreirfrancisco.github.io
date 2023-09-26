36cb683e8f321bc957debd89b45cdfd4aa6c8292
andreirfrancisco <132955328+andreirfrancisco@users.noreply.github.com>
2023-09-25 21:18:41 -0700

    Update website.js

--- website.js
+++ website.js
@@ -6,8 +6,15 @@ import {
 
 window.onload = function() {
 	let style_1 = document.querySelector('.Tab');
-	let style_2 = document.querySelector('.Subjects');
-	let style_3 = document.querySelector('.Main');
+let style_2 = document.querySelector('.Subjects');
+let style_3 = document.querySelector('.Main');
+let page = document.querySelector('.page');
+
+page.style.height = window.innerHeight + 'px';
+style_1.style.height = '100%';
+style_2.style.height = '100%';
+style_3.style.height = '100%';
+
 	default_page();
 }
 
@@ -17,9 +24,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	icons.forEach(icon => {
 		icon.addEventListener("click", function() {
 			const text = document.querySelector('.Sub-Icons');
+			// console.log("TEXT:" , text);
 			const iconId = parseInt(icon.id);
 			if (icon_to_sub.has(iconId)) {
 				const tabs = icon_to_sub.get(iconId);
+				// console.log("Tabs:", tabs);
 				displayTabs(tabs, text);
 				displaySub();
 			} else {
@@ -31,51 +40,111 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function handleClick(content) {
 	const maincontentElement = document.querySelector(".Main-Content");
-
+	console.log("MaincontentElement:", maincontentElement); 
 	if (content) {
 		maincontentElement.style.opacity = '0';
 		setTimeout(function() {
 			maincontentElement.innerHTML = '';
-
+			maincontentElement.style.textAlign = "left";
+				// need to edit this for specifc content
 			content.forEach(item => {
 				const paragraph = document.createElement('p');
+				console.log("Paragraph:", paragraph, "with", item);
 				paragraph.textContent = item;
+				paragraph.style.margin = "0";
 				maincontentElement.appendChild(paragraph);
 			});
-
+				console.log("Goes to the div-main:", maincontentElement);
 			maincontentElement.style.opacity = '1';
 		}, 300);
 	}
 }
+function first_icon(text,key) { 
+    const parentElement = document.querySelector(".Main-Content"); 
+	parentElement.style.backgroundcolorcolor = 'white';	
+	parentElement.innerHTML = '';	
+	const ucla_img = "tab_images/ucla.jpg"; 
+	const coursera_img = "tab_images/coursera.png"; 
+	const images = [ucla_img, coursera_img]; // Fixed the images array
+	if(key==1){
+		setTimeout(function() {
+		const box = document.createElement('div'); 
+		box.style.textAlign = "center"; 
+		box.classList.add('box'); 
+		// Loop through the images and text
+		for (let i = 0; i < images.length; i++) { 
+			const imgElement = document.createElement('img'); 
+			imgElement.src = images[i]; // Set the src attribute
+	
+			const school = document.createElement('p'); 
+			school.style.fontSize = "20px";
+			school.textContent = text[i * 2]; // Adjusted text index
+			const date = document.createElement('p'); 
+			date.textContent = text[i * 2 + 1]; // Adjusted text index
+			date.style.fontSize = "20px";
+			box.appendChild(imgElement); 
+			box.appendChild(school); 
+			box.appendChild(date); 
+		}
+	
+		parentElement.appendChild(box); 
+		console.log("First_icon", text[0]); }, 300 )
 
-function displayTabs(tabs, text) {
-	let content = "";
-	for (let i = 0; i < tabs.length; i++) {
-		content += `<p class='clickable' data-index='${i}'>${tabs[i]}</p>`;
 	}
-
-	text.innerHTML = content;
-
-	const clickableParagraphs = document.querySelectorAll('.clickable');
-
-	clickableParagraphs.forEach(paragraph => {
-		paragraph.addEventListener('click', function() {
-			const clickedTag = paragraph.textContent;
-			const key = Array.from(icon_to_sub.values()).flat().indexOf(clickedTag) + 1;
-
-			if (key > 0) {
-				const content = sub_to_main.get(key);
+	else if (key == 2) {
+      handleClick(text);
+    } 
+	else if (key == 3) {
+        handleClick(text);
+    } 
+	else if (key == 4) {
+        handleClick(text);
+    } 
+	else {
+        console.log("error");
+    }
+}
+function displayTabs(tabs, text) {
+    let content = "";
+    for (let i = 0; i < tabs.length; i++) {
+        content += `<p class='clickable' data-index='${i}'>${tabs[i]}</p>`;
+    }
+    text.innerHTML = content;
+    const clickableParagraphs = document.querySelectorAll('.clickable');
+    clickableParagraphs.forEach(paragraph => {
+        paragraph.addEventListener('click', function() {
+            const clickedTag = paragraph.textContent;
+            console.log("CLICKED TAG:", clickedTag);
+            const key = Array.from(icon_to_sub.values()).flat().indexOf(clickedTag) + 1;
+            console.log("Key in icon_to_sub:", key);
+            console.log("Content with sub_to_main:", sub_to_main);
+            const content = sub_to_main.get(key);
+            console.log("Key:", key , "with", content);
+            const headerElement = document.querySelector(".header");
+            console.log("Header Element:", headerElement); 
+            if (key <= 4) {
+                console.log("First Tags");
+                first_icon(content,key);
+                headerElement.innerHTML = clickedTag;
+            } else if (key > 4 && key < 8) {
+                console.log("Second Tags");
 				handleClick(content);
-				const headerElement = document.querySelector(".header");
-				headerElement.innerHTML = clickedTag;
-			}
-		});
-	});
+                headerElement.innerHTML = clickedTag;
+            } else if (key > 8 && key < 12) {
+                console.log("Third Tags");
+				handleClick(content);
+                headerElement.innerHTML = clickedTag;
+            } else {
+                console.log("Fourth Tags");
+				handleClick(content);
+                headerElement.innerHTML = clickedTag;
+            }
+        });
+    });
 }
 function displaySub() {
 	let style_1 = document.querySelector('.Subjects');
 	let style_2 = document.querySelector('.Main');
-	let mediaQuery = window.matchMedia('(max-width: 500px)'); // Change the media query to your desired condition
 	function applyStyles() {
 		if (style_1.style.width === '12%') {
 			style_1.style.width = '0';
